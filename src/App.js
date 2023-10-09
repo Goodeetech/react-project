49a13c14e70901964c7b7fb20125f7929ba6de4a
Goodeetech <ayomidegoodee@gmail.com>
2023-10-09 02:09:41 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+// import components
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" Component={Home}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="/cocktail/:id" Component={SingleCocktail}></Route>
+          <Route path="*" Component={Error}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <section className="section about section">
+        <h1 className="section-title">About Us</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
+          reprehenderit laudantium possimus doloribus exercitationem magni iusto
+          rerum aperiam ullam quaerat, aliquid, omnis pariatur cum dolores
+          facere. Magnam esse quasi voluptatum blanditiis soluta consequatur
+          distinctio expedita adipisci! Eligendi dignissimos error nulla.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
